17887a928f03737359fbd09866e34b583b87a546
LaonaGruenais <emmie.laona@gmail.com>
2022-04-02 15:39:02 +0200

    Page profil

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,26 @@
-import React from "react";
-import { Text } from "native-base";
+import React from 'react'
+import HeaderProfil from '../components/profile/HeaderProfil'
+import UserProfil from '../components/profile/UserProfil'
+import { TabsComponent } from '../components/tabs/Tabs'
 
-function ProfileScreen() {
+// Navigation dans le profil
+
+// fonction pour l'affichage du bon screen avec une route
+function ProfileScreen () {
   return (
-    <Text>PROFILE SCREEN</Text>
+    <>
+      <HeaderProfil />
+      <TabsComponent
+        views={[
+          {
+            key: 'first',
+            title: 'Profil',
+            component: UserProfil
+          }
+        ]}
+        style={{ marginTop: 30 }}
+      />
+    </>
   )
 }
 
